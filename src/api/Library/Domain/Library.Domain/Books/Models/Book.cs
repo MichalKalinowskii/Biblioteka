2bf8d6cc898c9aac74d3a7674c7b06816b84284a
MichalKalinowskii <kalinowskimichal638@gmail.com>
2025-04-06 22:28:22 +0200

    book service implementation

--- src/api/Library/Domain/Library.Domain/Books/Models/Book.cs
+++ src/api/Library/Domain/Library.Domain/Books/Models/Book.cs
@@ -19,7 +19,6 @@ public class Book
     private IBookPersistence bookPersistence;
 
     public Book(BookEntity book)
-    //IBookPersistence bookPersistence)
     {
         Id = Guid.NewGuid();
         Title = book.Title;
@@ -29,130 +28,10 @@ public class Book
         Description = book.Description;
         ISBN = book.ISBN;
         Publisher = book.Publisher;
-
-        //ValidateBook(book, bookPersistence);
-        //this.bookPersistence = bookPersistence;
     }
 
-    private void ValidateBook(BookEntity book, IBookPersistence bookPersistence)
+    public void ChangeGenre(string newGenreName)
     {
-        if (bookPersistence is null)
-        {
-            throw new DomainException("Passed IBookPersistence was null");
-        }
-
-        if (book == null)
-        {
-            throw new DomainException("Passed BookEntity was null");
-        }
-
-        if (string.IsNullOrWhiteSpace(book.Publisher))
-        {
-            throw new DomainException("No publisher given");
-        }
-
-        if (string.IsNullOrWhiteSpace(book.Title))
-        {
-            throw new DomainException("No book title given");
-        }
-
-        if (string.IsNullOrWhiteSpace(book.TitlePageImageUrl))
-        {
-            throw new DomainException("No title page image url given");
-        }
-
-        if (book.ReleaseDate == default)
-        {
-            throw new DomainException("No release date given");
-        }
-
-        if (string.IsNullOrWhiteSpace(book.ISBN))
-        {
-            throw new DomainException("No ISBN given");
-        }
-
-        if (book.ISBN.Length != 10 && book.ISBN.Length != 13)
-        {
-            throw new DomainException("ISBN must be 10 or 13 characters long");
-        }
-    }
-
-    public void ChangeGenre(Genre genre)
-    {
-        if (genre == null)
-        {
-            throw new DomainException("Genre is required");
-        }
-
-        if (string.IsNullOrWhiteSpace(genre.Name))
-        {
-            throw new DomainException("Genre name is required");
-        }
-
-        this.book.GenreId = genre.Id;
-        this.book.Genre = genre;
-    }
-
-    public void AddAuthors(List<AuthorEntity> authors)
-    {
-        if (!authors.Any())
-        {
-            throw new DomainException("No authors given");
-        }
-
-        this.book.Authors.AddRange(authors);
-    }
-
-    public void RemoveAuthors(List<int> authorIds)
-    {
-        if (!authorIds.Any())
-        {
-            throw new DomainException("No authors given");
-        }
-
-        this.book.Authors.RemoveAll(x => authorIds.Contains(x.Id));
-    }
-
-    public async Task<Result> Save()
-    {
-        ValidateGenre();
-        ValidateAuthors();
-
-        return await bookPersistence.Save(this.book);
-    }
-
-    private void ValidateGenre()
-    {
-        if (this.book.Genre is null)
-        {
-            throw new DomainException("Genre is required");
-        }
-        if (string.IsNullOrWhiteSpace(this.book.Genre.Name))
-        {
-            throw new DomainException("Genre name is required");
-        }
-    }
-
-    private void ValidateAuthors()
-    {
-        if (!this.book.Authors.Any())
-        {
-            throw new DomainException("No authors given");
-        }
-
-        if (this.book.Authors.Any(x => string.IsNullOrWhiteSpace(x.FirstName)))
-        {
-            throw new DomainException("Some authors first name was not given");
-        }
-
-        if (this.book.Authors.Any(x => string.IsNullOrWhiteSpace(x.LastName)))
-        {
-            throw new DomainException("Some authors last name was not given");
-        }
-
-        if (this.book.Authors.Any(x => x.BirthDate == default))
-        {
-            throw new DomainException("Some authors birth date was not given");
-        }
+        Genre = new Genre(newGenreName);
     }
 }
--- src/api/Library/Infrastructure/Library.Infrastructure/Domain/Books/BookRepository.cs
+++ src/api/Library/Infrastructure/Library.Infrastructure/Domain/Books/BookRepository.cs
@@ -1,14 +1,29 @@
 using Library.Domain.Books.Entites;
 using Library.Domain.Books.Interfaces;
+using Library.Domain.Books.Models;
 using Library.Domain.SeedWork;
 
 namespace Library.Infrastructure.Domain.Books;
 
 public class BookRepository : IBookPersistence
 {
-    public Task<Result> Save(BookEntity book)
+    public Task<Result> AddBookAsync(Book book, CancellationToken cancellationToken)
     {
-        //TODO: Implement
+        throw new NotImplementedException();
+    }
+
+    public Task<Book> GetBookByISBN(string ISBN, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Book> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result> UpdateBookAsync(Book book, CancellationToken cancellationToken)
+    {
         throw new NotImplementedException();
     }
 }
